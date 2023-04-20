44421aabc883bb098d335aa39b12450f2047d717
Andra <andradev0305@gmail.com>
2023-04-20 13:54:19 +0300

    fixed tooltip issue

--- src/components/shop/NavigationBar.js
+++ src/components/shop/NavigationBar.js
@@ -4,40 +4,41 @@ import { ReactComponent as IconGrid } from "../../assets/svgs/a-icons-24-color-g
 import RoleTypeSelect from "./RoleTypeSelect";
 import classNames from "classnames";
 import { Tooltip } from "react-tooltip";
+import "react-tooltip/dist/react-tooltip.css";
 
 const NavigationBar = ({ handleViewType, selectedView, viewType }) => {
   return (
     <section className="nav-section">
       <RoleTypeSelect />
       <div className="view-icons">
-        <IconGrid
-          onClick={() => handleViewType(viewType.grid)}
-          className={classNames("view-icon", {
-            active: selectedView[viewType.grid],
-          })}
-          data-tooltip-id="icon-grid"
-          data-tooltip-content="Grid"
-        />
+        <div id="icon-grid">
+          <IconGrid
+            onClick={() => handleViewType(viewType.grid)}
+            className={classNames("view-icon", {
+              active: selectedView[viewType.grid],
+            })}
+          />
+        </div>
         <Tooltip
-          id="icon-grid"
-          aria-haspopup="true"
+          anchorSelect="#icon-grid"
           place="bottom"
           effect="solid"
+          content="Grid"
           className="icon-tooltip-wrapper"
         />
-        <IconCard
-          onClick={() => handleViewType(viewType.card)}
-          className={classNames("view-icon", {
-            active: selectedView[viewType.card],
-          })}
-          data-tooltip-id="icon-card"
-          data-tooltip-content="Card"
-        />
+        <div id="icon-card">
+          <IconCard
+            onClick={() => handleViewType(viewType.card)}
+            className={classNames("view-icon", {
+              active: selectedView[viewType.card],
+            })}
+          />
+        </div>
         <Tooltip
-          id="icon-card"
-          aria-haspopup="true"
+          anchorSelect="#icon-card"
           place="bottom"
           effect="solid"
+          content="Card"
           className="icon-tooltip-wrapper"
         />
       </div>
